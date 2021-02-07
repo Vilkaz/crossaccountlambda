b982d30bfdc3458d9a3bae79208ca608a5e66c2d
vilius.kukanauskas@gmail.com <GitHubCom123123123!!!>
2021-02-07 15:18:12 +0100

    tests working with user create and user delete

--- src/main/java/de/vilkas/controller/IAMUserController.java
+++ src/main/java/de/vilkas/controller/IAMUserController.java
@@ -1,18 +1,24 @@
 package de.vilkas.controller;
 
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
-import com.amazonaws.services.identitymanagement.model.CreateUserResult;
-import software.amazon.awssdk.services.iam.model.CreateUserRequest;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.CreateUserRequest;
+import com.amazonaws.services.identitymanagement.model.CreateUserResult;
+import com.amazonaws.services.identitymanagement.model.DeleteUserRequest;
+import com.amazonaws.services.identitymanagement.model.DeleteUserResult;
 
 public class IAMUserController {
 
-    private static final String SUFFIX = "-user" ;
+    private static final String SUFFIX = "-user";
 
-    public void createCrossAccountUser(String processname) {
+    public CreateUserResult createCrossAccountUser(String processname) {
         String username = processname + SUFFIX;
+
         final AmazonIdentityManagement iam =
-                AmazonIdentityManagementClientBuilder.defaultClient();
+                AmazonIdentityManagementClientBuilder
+                        .standard()
+                        .withRegion("eu-central-1")
+                        .build();
 
         CreateUserRequest request = new CreateUserRequest()
                 .withUserName(username);
@@ -21,6 +27,33 @@ public class IAMUserController {
 
         System.out.println("Successfully created user: " +
                 response.getUser().getUserName());
+
+        return response;
+    }
+
+    public DeleteUserResult deleteCrossAccountUser(String processname) {
+        String username = processname + SUFFIX;
+
+        final AmazonIdentityManagement iam =
+                AmazonIdentityManagementClientBuilder
+                        .standard()
+                        .withRegion("eu-central-1")
+                        .build();
+
+        DeleteUserRequest deleteUserRequest = new DeleteUserRequest()
+                .withUserName(username);
+
+
+
+//        CreateUserRequest request = new CreateUserRequest()
+//                .withUserName(username);
+
+        DeleteUserResult response = iam.deleteUser(deleteUserRequest);
+
+        System.out.println("Successfully deleted user: " +
+                response.toString());
+
+        return response;
     }
 
 }
---
+++ src/test/java/de/vilkas/IT/IAMUserTest.java
@@ -0,0 +1,59 @@
+package de.vilkas.IT;
+
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.ListUsersRequest;
+import com.amazonaws.services.identitymanagement.model.ListUsersResult;
+import com.amazonaws.services.identitymanagement.model.User;
+import de.vilkas.controller.IAMUserController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.iam.model.GetUserRequest;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class IAMUserTest {
+
+    @Test
+    public void createAndDeleteUserByName() {
+        String name = "Testuser1337";
+        IAMUserController controller = new IAMUserController();
+        //create a new iam user
+        controller.createCrossAccountUser(name);
+        //search for iam user by name on the AWS account
+        GetUserRequest getUserRequest = GetUserRequest
+                .builder()
+                .userName(name)
+                .build();
+
+        final AmazonIdentityManagement iam =
+                AmazonIdentityManagementClientBuilder
+                        .standard()
+                        .withRegion("eu-central-1")
+                        .build();
+
+        ListUsersRequest request = new ListUsersRequest();
+        ListUsersResult response = iam.listUsers(request);
+        Optional<User> testUser = response.getUsers().stream()
+                .filter(testuserExists(name))
+                .findAny();
+
+        Assertions.assertTrue(testUser.isPresent());
+
+        controller.deleteCrossAccountUser(name);
+
+        request = new ListUsersRequest();
+        response = iam.listUsers(request);
+        testUser = response.getUsers().stream()
+                .filter(testuserExists(name))
+                .findAny();
+
+        Assertions.assertFalse(testUser.isPresent());
+    }
+
+    private Predicate<User> testuserExists(String s) {
+        String testUser = s + "-user";
+        return u -> testUser.equals(u.getUserName());
+    }
+}
